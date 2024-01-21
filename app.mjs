ba1b91ac951b602aa1720db3fb07b579c119ecb5
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-01-21 18:17:05 +0500

    final

---
+++ app.mjs
@@ -0,0 +1,42 @@
+const inputbox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+const addtask = () =>
+{
+    if(inputbox.value === "")
+    {
+        alert("You must write something");
+    }
+    else
+    {
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+    saveData();
+}
+listContainer.addEventListener("click", function(e)
+{
+    if (e.target.tagName === "LI") 
+    {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN")
+    {
+        e.target.parentElement.remove();
+    }
+
+}, false);
+const saveData = () =>
+{
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+const showtask = () =>
+{
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
